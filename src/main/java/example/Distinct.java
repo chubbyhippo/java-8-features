22b4dcd13a7779f49cc8ef68c0675c77bee2ccc8
ch <mkmsacc@outlook.com>
2021-10-30 13:44:12 +0700

    Add Distinct

---
+++ src/main/java/example/Distinct.java
@@ -0,0 +1,11 @@
+package example;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Distinct {
+    public static void main(String[] args) {
+        Stream.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 6, 6, 7, 8, 8, 8, 9).distinct()
+                .collect(Collectors.toList()).forEach(System.out::println);
+    }
+}
