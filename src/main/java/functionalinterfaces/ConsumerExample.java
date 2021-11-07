219eebf586b827661c2a894473d9945647350d5d
ch <mkmsacc@outlook.com>
2021-11-07 14:40:12 +0700

    Add printNameAndActivities

--- src/main/java/functionalinterfaces/ConsumerExample.java
+++ src/main/java/functionalinterfaces/ConsumerExample.java
@@ -1,17 +1,27 @@
 package functionalinterfaces;
 
+import data.Student;
 import data.StudentDataBase;
+
 import java.util.function.Consumer;
 
 public class ConsumerExample {
     public static void printName() {
-        Consumer consumer = System.out::println;
-        StudentDataBase .getAllStudents().forEach(consumer);
-
+        Consumer<Student> consumer = System.out::println;
+        StudentDataBase.getAllStudents().forEach(consumer);
     }
+
+    public static void printNameAndActivities() {
+        Consumer<Student> c1 = student -> System.out.print(student.getName());
+        Consumer<Student> c2 = student -> System.out.println(student.getActivities());
+        StudentDataBase.getAllStudents()
+                .forEach(c1.andThen(c2));
+    }
+
     public static void main(String[] args) {
         Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
         consumer.accept("hello");
         printName();
+        printNameAndActivities();
     }
 }
