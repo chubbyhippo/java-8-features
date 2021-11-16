a4b950792c59b5ab9d3f358419054e41ded1c630
ch <mkmsacc@outlook.com>
2021-11-16 09:58:55 +0700

    Refactor code

--- src/main/java/functionalinterfaces/PredicateAndConsumerExample.java
+++ src/main/java/functionalinterfaces/PredicateAndConsumerExample.java
@@ -9,12 +9,13 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class PredicateAndConsumerExample {
-    static Predicate<Student> p1 = student -> student.getGradeLevel() >= 3;
-    static Predicate<Student> p2 = student -> student.getGpa() >= 3.9;
-    static BiConsumer<String, List<String>> biConsumer
+    Predicate<Student> p1 = student -> student.getGradeLevel() >= 3;
+    Predicate<Student> p2 = student -> student.getGpa() >= 3.9;
+
+    BiConsumer<String, List<String>> biConsumer
             = (name, activities) -> System.out.println(name + " : " + activities);
 
-    static Consumer<Student> studentConsumer = student -> {
+    Consumer<Student> studentConsumer = student -> {
         if (p1.and(p2).test(student)) {
             biConsumer.accept(student.getName(), student.getActivities());
         }
