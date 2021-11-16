6d7482534e067d9872d31394ed94abdadf4ba8d0
ch <mkmsacc@outlook.com>
2021-11-16 09:52:11 +0700

    Add PredicateAndConsumerExample

---
+++ src/main/java/functionalinterfaces/PredicateAndConsumerExample.java
@@ -0,0 +1,32 @@
+package functionalinterfaces;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PredicateAndConsumerExample {
+    static Predicate<Student> p1 = student -> student.getGradeLevel() >= 3;
+    static Predicate<Student> p2 = student -> student.getGpa() >= 3.9;
+    static BiConsumer<String, List<String>> biConsumer
+            = (name, activities) -> {
+        System.out.println(name + " : " + activities);
+    };
+
+    static Consumer<Student> studentConsumer = student -> {
+        if (p1.and(p2).test(student)) {
+            biConsumer.accept(student.getName(), student.getActivities());
+        }
+    };
+
+    public void printNameAndActivities(List<Student> students) {
+       students.forEach(studentConsumer);
+    }
+
+    public static void main(String[] args) {
+        new PredicateAndConsumerExample().printNameAndActivities(StudentDataBase.getAllStudents());
+    }
+}
