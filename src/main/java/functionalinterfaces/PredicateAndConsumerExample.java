32e479bdadc8e1f69bcbc9d017d61d422b48f3dd
ch <mkmsacc@outlook.com>
2021-11-16 10:37:30 +0700

    Add BiPredicate

--- src/main/java/functionalinterfaces/PredicateAndConsumerExample.java
+++ src/main/java/functionalinterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,11 +13,18 @@ public class PredicateAndConsumerExample {
     Predicate<Student> p1 = student -> student.getGradeLevel() >= 3;
     Predicate<Student> p2 = student -> student.getGpa() >= 3.9;
 
+    BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) ->
+            gradeLevel >= 3 && gpa >= 3.9;
+
     BiConsumer<String, List<String>> biConsumer
             = (name, activities) -> System.out.println(name + " : " + activities);
 
     Consumer<Student> studentConsumer = student -> {
-        if (p1.and(p2).test(student)) {
+//        if (p1.and(p2).test(student)) {
+//            biConsumer.accept(student.getName(), student.getActivities());
+//        }
+        if (biPredicate.test(student.getGradeLevel(),
+                student.getGpa())) {
             biConsumer.accept(student.getName(), student.getActivities());
         }
     };
