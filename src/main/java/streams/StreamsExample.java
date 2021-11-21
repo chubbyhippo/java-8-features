622274a7d403ea594269f6a6cca4acdd24c4fb0a
ch <mkmsacc@outlook.com>
2021-11-21 10:09:20 +0700

    Add peeks

--- src/main/java/streams/StreamsExample.java
+++ src/main/java/streams/StreamsExample.java
@@ -14,9 +14,12 @@ public class StreamsExample {
         Predicate<Student> studentGpaPredicate = student -> student.getGpa() >= 3.9;
         Map<String, List<String>> studentMap = StudentDataBase.getAllStudents()
                 .stream()
+                .peek(student -> System.out.println("stream : " + student))
                 .parallel()
                 .filter(studentPredicate)
+                .peek(student -> System.out.println("after 1st filter : " + student))
                 .filter(studentGpaPredicate)
+                .peek(student -> System.out.println("after 2nd filter : " + student))
                 .collect(Collectors.toMap(Student::getName, Student::getActivities));
 
         System.out.println(studentMap);
