8f2d731ceba94e926a5bb833776754b9e0b15cc0
ch <mkmsacc@outlook.com>
2021-11-22 11:08:37 +0700

    Add StreamsMinExample

---
+++ src/main/java/streams/StreamsMinExample.java
@@ -0,0 +1,32 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamsMinExample {
+    public static int findMinValue(List<Integer> integers) {
+        return integers
+                .stream()
+                .reduce(Integer.MAX_VALUE, (integer, integer2) -> integer < integer2 ? integer : integer2);
+    }
+
+    public static Optional<Integer> findMinValueOptional(List<Integer> integers) {
+        return integers
+                .stream()
+                .reduce((integer, integer2) -> integer < integer2 ? integer : integer2);
+    }
+
+    public static void main(String[] args) {
+        var integers = Arrays.asList(874, 232, 772, 844, 931);
+        System.out.println(findMinValue(integers));
+
+        var minValueOptional = findMinValueOptional(integers);
+        if (minValueOptional.isPresent()) {
+            System.out.println(minValueOptional);
+            System.out.println(minValueOptional.get());
+        } else {
+            System.out.println("Input list is empty");
+        }
+    }
+}
