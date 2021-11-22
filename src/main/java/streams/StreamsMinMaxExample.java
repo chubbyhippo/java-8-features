c51050344a0c485df2898269eca9dec5b2cb8224
ch <mkmsacc@outlook.com>
2021-11-22 10:54:24 +0700

    Add StreamsMinMaxExample

---
+++ src/main/java/streams/StreamsMinMaxExample.java
@@ -0,0 +1,32 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamsMinMaxExample {
+    public static int findMaxValue(List<Integer> integers) {
+        return integers
+                .stream()
+                .reduce(0, (integer, integer2) -> integer > integer2 ? integer : integer2);
+    }
+
+    public static Optional<Integer> findMaxValueOptional(List<Integer> integers) {
+        return integers
+                .stream()
+                .reduce((integer, integer2) -> integer > integer2 ? integer : integer2);
+    }
+
+    public static void main(String[] args) {
+        var integers = Arrays.asList(874, 232, 772, 844, 931);
+        System.out.println(findMaxValue(integers));
+
+        var maxValueOptional = findMaxValueOptional(integers);
+        if (maxValueOptional.isPresent()) {
+            System.out.println(maxValueOptional);
+            System.out.println(maxValueOptional.get());
+        } else {
+            System.out.println("Input list is empty");
+        }
+    }
+}
