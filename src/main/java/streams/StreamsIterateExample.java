37c141d455beca1442e5c546286aa134a7b27145
ch <mkmsacc@outlook.com>
2021-11-23 08:01:09 +0700

    Add limit

--- src/main/java/streams/StreamsIterateExample.java
+++ src/main/java/streams/StreamsIterateExample.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 public class StreamsIterateExample {
     public static void main(String[] args) {
         Stream.iterate(1, integer -> integer*2)
+                .limit(10)
                 .forEach(System.out::println);
     }
 }
