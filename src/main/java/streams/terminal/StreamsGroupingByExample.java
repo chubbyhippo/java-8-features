ab1c2ebb7debb2f4cbe111844b73a8d7dc8ad90a
ch <mkmsacc@outlook.com>
2021-11-25 08:27:26 +0700

    Add StreamsGroupingByExample

---
+++ src/main/java/streams/terminal/StreamsGroupingByExample.java
@@ -0,0 +1,22 @@
+package streams.terminal;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamsGroupingByExample {
+    public static void groupStudentsByGender() {
+        Map<String, List<Student>> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGender));
+
+        System.out.println(studentMap);
+    }
+
+    public static void main(String[] args) {
+        groupStudentsByGender();
+    }
+}
